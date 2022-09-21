74f36b031b0e36b1b363552fa29ca61544220716
TheWisePigeon <zozozozeph@gmail.com>
2022-09-21 14:28:00 +0000

    feat: added announcement mock

--- components/announcements.tsx
+++ components/announcements.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+
 export default function Announcements(){
+    const currentAnnouncement = useState(0)
+
+
     return (
-        <>
-        
-        </>
+        <div className=" m-auto w-3/5 h-40 lg:w-96">
+            <div>
+                <img src="https://picsum.photos/200/200" className=" lg:w-96" alt="" />
+            </div>
+            <div className=" flex justify-between ">
+                <button>
+                    {"<"}
+                </button>
+                <button>
+                    {">"}
+                </button>
+            </div>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Header from '../components/header'
 import Menu from '../components/menu'
+import Announcements from '../components/announcements'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header/>
-      <Menu/>
+      {/* <Menu/> */}
+      <Announcements/>
     </>
   )
 }
